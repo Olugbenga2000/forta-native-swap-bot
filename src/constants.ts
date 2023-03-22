299fe3b1ff5907cc54b90883322fed48334a86db
Olugbenga <ayoolaolugbenga38@yahoo.com>
2023-03-22 18:52:49 +0100

    chore: modularize constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const TOKEN_TRANSFER_EVENT: string = "event Transfer(address indexed from, address indexed to, uint256 value)";
+
+export const THRESHOLD_PERCENT = 3;
+export const MAX_TIMESTAMP = 30 * 60; // maximum time between concurrent swaps. 
+
+
