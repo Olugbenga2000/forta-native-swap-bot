f5d61ff7114f2a44b556cd487d6f04fa8af60be1
Olugbenga <ayoolaolugbenga38@yahoo.com>
2023-03-31 18:50:41 +0100

    removed axios support and increased jest timeout

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["dist"],
-  moduleNameMapper: {
-    axios: "axios/dist/node/axios.cjs"
-  }
+  testTimeout:  10000
 };
