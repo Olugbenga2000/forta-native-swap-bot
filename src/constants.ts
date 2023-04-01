b33ca7fc6181b3642abb4c1747b070363f3a6420
Olugbenga <ayoolaolugbenga38@yahoo.com>
2023-04-01 03:04:47 +0100

    added abi for chainlink aggregator interface and dollar threshold

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,10 @@
 export const MINIMUM_SWAP_COUNT = 2;
 export const ERC20_TRANSFER_EVENT = "event Transfer(address indexed from, address indexed to, uint256 value)";
 export const LOW_NONCE_THRESHOLD = 150;
-export const MIN_ETH_THRESHOLD = 30;
+export const MIN_ETH_THRESHOLD = 55000;
+export const MIN_DOLLAR_THRESHOLD = 55000;
 export const MAX_MINUTES_BETWEEN_SWAPS = 30;
+export const AGGREGATORV3_ABI = [
+    "function latestRoundData()external view returns (uint80 roundId,int256 answer,uint256 startedAt,uint256 updatedAt,uint80 answeredInRound)",
+    "function decimals() external view returns (uint8)"
+];
