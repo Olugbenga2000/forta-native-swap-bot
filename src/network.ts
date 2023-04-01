dad7045e841506c3db431b0dfb919c3ec9f0b849
Olugbenga <ayoolaolugbenga38@yahoo.com>
2023-04-01 22:02:38 +0100

    finished the getLatestPriceFeed function

--- src/network.ts
+++ src/network.ts
@@ -8,37 +8,37 @@ export interface NetworkData {
   }
   
   let MAINNET_DATA: NetworkData = {
-    nativeUsdAggregator:  "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+    nativeUsdAggregator:  "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",   // ETH/USD
     minNativeThreshold: "30",
   };
   
   let POLYGON_MAINNET_DATA: NetworkData = {
-    nativeUsdAggregator: "0xAB594600376Ec9fD91F8e885dADF0CE036862dE0",
+    nativeUsdAggregator: "0xAB594600376Ec9fD91F8e885dADF0CE036862dE0",    //MATIC/USD
     minNativeThreshold: "49139",
   };
   
   let ARBITRUM_MAINNET_DATA: NetworkData = {
-    nativeUsdAggregator: "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612",
+    nativeUsdAggregator: "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612",   // ETH/USD
     minNativeThreshold: "30"
   };
   
   let OPTIMISM_MAINNET_DATA: NetworkData = {
-    nativeUsdAggregator: "0x13e3Ee699D1909E989722E753853AE30b17e08c5",
+    nativeUsdAggregator: "0x13e3Ee699D1909E989722E753853AE30b17e08c5",    // ETH/USD
     minNativeThreshold: "30"
   };
   
   let AVALANCHE_DATA: NetworkData = {
-    nativeUsdAggregator: "0x0A77230d17318075983913bC2145DB16C7366156",
+    nativeUsdAggregator: "0x0A77230d17318075983913bC2145DB16C7366156",   // AVAX/USD
     minNativeThreshold: "3096"
   };
   
   let FANTOM_DATA: NetworkData = {
-    nativeUsdAggregator: "0xf4766552D15AE4d256Ad41B6cf2933482B0680dc",
+    nativeUsdAggregator: "0xf4766552D15AE4d256Ad41B6cf2933482B0680dc",   // FTM/USD
     minNativeThreshold: "115045",
   };
   
   const BNBCHAIN_DATA: NetworkData = {
-    nativeUsdAggregator: "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE",
+    nativeUsdAggregator: "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE",   // BNB/USD
     minNativeThreshold: "173",
   };
   
@@ -71,7 +71,8 @@ export interface NetworkData {
       }
     }
 
-    public async updateThreshold(chainId: number, provider: ethers.providers.Provider){
+    public async getLatestPriceFeed(provider: ethers.providers.Provider){
+        const { chainId } = await provider.getNetwork();
         const aggregatorContract  = new ethers.Contract(this.nativeUsdAggregator, AGGREGATORV3_ABI, provider);
         try {
             const [roundData, decimals] = await Promise.all([
@@ -81,8 +82,6 @@ export interface NetworkData {
             const price = toBn(roundData.answer.toString()).div(10**decimals);
             NETWORK_MAP[chainId].minNativeThreshold = toBn(MIN_DOLLAR_THRESHOLD).div(price).toFixed(2);
             this.setNetwork(chainId);
-            console.log(price.toString())
-            console.log(this.minNativeThreshold);
         } catch (error) {
             console.log("Error while fetching latest data: ", error);
         }
