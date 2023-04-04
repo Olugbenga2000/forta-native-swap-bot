5138fba8e779d5b9f87e19745868b395dd1ec956
Olugbenga <ayoolaolugbenga38@yahoo.com>
2023-04-04 10:55:42 +0100

    prettier

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export const MIN_DOLLAR_THRESHOLD = 55000;
 export const MAX_MINUTES_BETWEEN_SWAPS = 30;
 export const AGGREGATORV3_ABI = [
   "function latestRoundData()external view returns (uint80 roundId,int256 answer,uint256 startedAt,uint256 updatedAt,uint80 answeredInRound)",
-  "function decimals() external view returns (uint8)"
+  "function decimals() external view returns (uint8)",
 ];
--- src/network.spec.ts
+++ src/network.spec.ts
@@ -1,27 +1,27 @@
 import NetworkManager, { NETWORK_MAP } from "./network";
 
-const TEST_CHAIN_IDS: number[] = [1, 137, 42161, 10, 43114, 250, 56]
+const TEST_CHAIN_IDS: number[] = [1, 137, 42161, 10, 43114, 250, 56];
 describe("NetworkManager Test Suite", () => {
-    describe("tests for setNetwork function call", () => {
-        it("should correctly return native usd aggregator and min native threshold for each network", async () => {
-            for (let testChainId of TEST_CHAIN_IDS) {
-                const networkManager = new NetworkManager();
-                const { nativeUsdAggregator: mappedNativeUsdAggregator, minNativeThreshold: mappedMinNativeThreshold } = NETWORK_MAP[testChainId];
-                networkManager.setNetwork(testChainId);
-                expect(networkManager.nativeUsdAggregator).toStrictEqual(mappedNativeUsdAggregator);
-                expect(networkManager.minNativeThreshold).toStrictEqual(mappedMinNativeThreshold);
-            }
-        });
-    
-        it("should throw error when running bot on an unsupported network", async () => {
-            const networkManager = new NetworkManager();
-            expect(() => {
-                // 73 is an unsupported networkId
-                networkManager.setNetwork(73);
-            }).toThrow(new Error("Network not supported"));
-    
-        });
-    
+  describe("tests for setNetwork function call", () => {
+    it("should correctly return native usd aggregator and min native threshold for each network", async () => {
+      for (let testChainId of TEST_CHAIN_IDS) {
+        const networkManager = new NetworkManager();
+        const {
+          nativeUsdAggregator: mappedNativeUsdAggregator,
+          minNativeThreshold: mappedMinNativeThreshold,
+        } = NETWORK_MAP[testChainId];
+        networkManager.setNetwork(testChainId);
+        expect(networkManager.nativeUsdAggregator).toStrictEqual(mappedNativeUsdAggregator);
+        expect(networkManager.minNativeThreshold).toStrictEqual(mappedMinNativeThreshold);
+      }
     });
 
+    it("should throw error when running bot on an unsupported network", async () => {
+      const networkManager = new NetworkManager();
+      expect(() => {
+        // 73 is an unsupported networkId
+        networkManager.setNetwork(73);
+      }).toThrow(new Error("Network not supported"));
+    });
+  });
 });
