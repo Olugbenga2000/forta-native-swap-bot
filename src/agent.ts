d131361a0e4c797daa3bf3463f7db863ac1eeb36
Olugbenga <ayoolaolugbenga38@yahoo.com>
2023-04-10 23:54:16 +0100

    replaced block balance check with weth withdrawal event check

--- src/agent.ts
+++ src/agent.ts
@@ -1,17 +1,18 @@
 import { Finding, HandleTransaction, TransactionEvent, getEthersProvider, ethers } from "forta-agent";
-import { createOrUpdateData, toBn, toCs, deleteRedundantData, addTxToQueue } from "./utils";
+import { createOrUpdateData, toBn, toCs, deleteRedundantData} from "./utils";
 import { AddressRecord } from "./swap";
 import { createNewFinding } from "./finding";
-import { MINIMUM_SWAP_COUNT, ERC20_TRANSFER_EVENT, LOW_NONCE_THRESHOLD, BLOCK_DELAY } from "./constants";
+import { MINIMUM_SWAP_COUNT, ERC20_TRANSFER_EVENT, LOW_NONCE_THRESHOLD} from "./constants";
 import NetworkManager from "./network";
 
 const networkManager = new NetworkManager();
 export let totalNativeSwaps = 0;
 let unusualNativeSwaps = 0;
-let prevClearTimestamp = 0
+let prevClearTimestamp = 0;
+let chainId: number;
 
 export const initialize = (provider: ethers.providers.Provider) => async () => {
-  const { chainId } = await provider.getNetwork();
+  chainId = (await provider.getNetwork()).chainId;
   networkManager.setNetwork(chainId);
 };
 
@@ -20,16 +21,12 @@ export const provideBotHandler = (
   provider: ethers.providers.JsonRpcProvider,
   lowTxCount: number,
   swapCountThreshold: number,
-  network: NetworkManager,
-  blockDelay: number
+  network: NetworkManager
 ): HandleTransaction => async (txEvent: TransactionEvent): Promise<Finding[]> => {
   const findings: Finding[] = [];
-  let txEventOrUndefined = addTxToQueue(txEvent, blockDelay);
-  if (!txEventOrUndefined) return findings;
-  txEvent = txEventOrUndefined;
   const { from, hash, timestamp, blockNumber } = txEvent;
-  // remove redundant data from the AddressRecord Map and get latest price from chainlink oracle every 10000 blocks
 
+  // remove redundant data from the AddressRecord Map and get latest price from chainlink oracle every 10000 blocks
   if (blockNumber % 10000 === 0 && timestamp !== prevClearTimestamp) {
     prevClearTimestamp = timestamp;
     deleteRedundantData(timestamp);
@@ -54,18 +51,21 @@ export const provideBotHandler = (
       return true;
     });
   if (!erc20TransferEventsFromMsgSender.length) return findings;
-  // Compare account balance at previous block to balance at current block to determine if a swap occured
-  const [previousBalance, currentBalance, nonce] = await Promise.all([
-    provider.getBalance(msgSender, blockNumber - 1),
-    provider.getBalance(msgSender, blockNumber),
-    provider.getTransactionCount(msgSender, blockNumber),
-  ]);
-  const ethBalanceDiff = toBn(currentBalance.toString()).minus(toBn(previousBalance.toString()));
-  if (ethBalanceDiff.lte(0)) return findings;
+  // Check for withdrawals from the wrapped native token to determine if a native swap occurred
+  let wethWithdrawals;
+  if (chainId === 42161) 
+  wethWithdrawals = erc20TransferEvents.filter(log => log.address = network.wNative && log.args.to === ethers.constants.AddressZero)
+  else
+  wethWithdrawals = txEvent.filterLog(wethWithdrawalEvent, network.wNative);
+  if (!wethWithdrawals.length) return findings;
   totalNativeSwaps++;
+  const ethWithdrawn = wethWithdrawals.reduce((acc, log) => toBn(log.args.wad).plus(acc), toBn(0));
+  
+
   // Check if msg.sender's address is new
+  const nonce = await provider.getTransactionCount(msgSender);
   if (nonce > lowTxCount) return findings;
-  createOrUpdateData(ethBalanceDiff, hash, msgSender, blockNumber, timestamp, erc20TransferEventsFromMsgSender);
+  createOrUpdateData(ethWithdrawn, hash, msgSender, blockNumber, timestamp, erc20TransferEventsFromMsgSender);
   const addressRecord = AddressRecord.get(msgSender);
   /**
    * create a finding if total eth received by the sender is greater than the threshold AND if
@@ -92,7 +92,6 @@ export default {
     getEthersProvider(),
     LOW_NONCE_THRESHOLD,
     MINIMUM_SWAP_COUNT,
-    networkManager,
-    BLOCK_DELAY
+    networkManager
   ),
 };
